e15f2863cb2b7d223fea3f77a84e0785257d4cc6
jpbellizia <jpaulo_6@hotmail.com>
2026-07-24 19:39:41 -0300

    Feat: New functions (reverse-rotate and rotate

--- srcs/utils/ops_utils.c
+++ srcs/utils/ops_utils.c
@@ -6,7 +6,7 @@
 /*   By: jpaulo-p <jpaulo-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/24 16:14:15 by jpaulo-p          #+#    #+#             */
-/*   Updated: 2026/07/24 16:55:19 by jpaulo-p         ###   ########.fr       */
+/*   Updated: 2026/07/24 19:30:50 by jpaulo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,54 @@ void	swap(t_stack *stack)
 {
 	int	temp;
 
-    if (!stack || stack->size < 2)
-    {
-        return ;
-    }
-    temp = stack->values[0];
+	if (!stack || stack->size < 2)
+		return ;
+	temp = stack->values[0];
 	stack->values[0] = stack->values[1];
 	stack->values[1] = temp; 
 }
 
 void	push(t_stack *stack, int value)
 {
-    
+	
 }
 
 void	rotate(t_stack *stack)
 {
-    
+	int	temp;
+	int	i;
+
+	if (!stack || stack->size < 2)
+		return ;
+	i = 0;
+	temp = stack->values[0];
+	while (i < stack->size - 1)
+	{
+		stack->values[i] = stack->values[i + 1];
+		i++;
+	}
+	stack->values[stack->size - 1] = temp;
 }
 
 void	reverse_rotate(t_stack *stack)
 {
-    
+	int	temp;
+	int	i;
+
+	if (!stack || stack->size < 2)
+		return ;
+	i = stack->size - 1;
+	temp = stack->values[stack->size - 1];
+	while (i > 0)
+	{
+		stack->values[i] = stack->values[i - 1];
+		i--;
+	}
+	stack->values[0] = temp;
 }
 
 int		pop(t_stack	*stack)
 {
-    
+	int	value;
+	int	i;
 }
