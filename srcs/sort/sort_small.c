9711a6a46c79bc29f1f1ffdb64c99d8ba412b57a
jpbellizia <jpaulo_6@hotmail.com>
2026-08-01 01:04:13 -0300

    Fix: update some function files and implemente sort_four

--- srcs/sort/sort_small.c
+++ srcs/sort/sort_small.c
@@ -6,11 +6,20 @@
 /*   By: jpaulo-p <jpaulo-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 13:10:26 by vneves-c          #+#    #+#             */
-/*   Updated: 2026/07/31 14:39:30 by jpaulo-p         ###   ########.fr       */
+/*   Updated: 2026/08/01 01:03:11 by jpaulo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	sort_two(t_context *ctx)
+{
+	int	*v;
+
+	v = ctx->a.values;
+	if (v[0] > v[1])
+		sa(ctx);
+}
 
 void	sort_three(t_context *ctx)
 {
@@ -37,13 +46,12 @@ void	sort_three(t_context *ctx)
 	}
 }
 
-void	sort_two(t_context *ctx)
+void	sort_four(t_context *ctx)
 {
-	int	*v;
-
-	v = ctx->a.values;
-	if (v[0] > v[1])
-		sa(ctx);
+	rotate_a_to_top(ctx, find_min_index(&ctx->a));
+	pb(ctx);
+	sort_three(ctx);
+	pa(ctx);
 }
 
 void	sort_five(t_context *ctx)
@@ -55,45 +63,4 @@ void	sort_five(t_context *ctx)
 	sort_three(ctx);
 	pa(ctx);
 	pa(ctx);
-}
-
-int	find_min_index(t_stack *stack)
-{
-	int	min_index;
-	int	i;
-	
-	i = 1;
-	min_index = 0;
-	while (i < stack->size)
-	{
-		if (stack->values[i] < stack->values[min_index])
-			min_index = i;
-		i++;
-	}
-	return (min_index);
-}
-
-void	move_min_to_top_a(t_context *ctx)
-{
-	int	min_index;
-	int	i;
-
-	min_index = find_min_index(&ctx->a);
-	i = 0;
-	if (min_index <= ctx->a.size / 2)
-	{
-		while (i < min_index)
-		{
-			ra(ctx);
-			i++;
-		}
-	}
-	else
-	{
-		while (i < ctx->a.size - min_index)
-		{
-			rra(ctx);
-			i++;
-		}
-	}
 }
