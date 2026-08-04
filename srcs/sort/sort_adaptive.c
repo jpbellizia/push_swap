972dfd2cb905cb7cbc3d2aae52b250ba15ca1919
jpbellizia <jpaulo_6@hotmail.com>
2026-08-04 14:34:06 -0300

    Feat: New function: sort_adaptive.c

--- srcs/sort/sort_adaptive.c
+++ srcs/sort/sort_adaptive.c
@@ -6,8 +6,19 @@
 /*   By: jpaulo-p <jpaulo-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 13:10:26 by vneves-c          #+#    #+#             */
-/*   Updated: 2026/07/30 15:21:18 by jpaulo-p         ###   ########.fr       */
+/*   Updated: 2026/08/04 14:33:40 by jpaulo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	sort_adaptive(t_context *ctx)
+{
+	ctx->disorder = compute_disorder(&ctx->a);
+	if (ctx->disorder < 2000)
+		sort_simple(ctx);
+	else if (ctx->disorder < 5000)
+		sort_medium(ctx);
+	else
+		sort_complex(ctx);
+}
