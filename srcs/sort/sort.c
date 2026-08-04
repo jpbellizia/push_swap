4803f9c8e97998cb8e4c034a0c206a788b4a5e5a
jpbellizia <jpaulo_6@hotmail.com>
2026-08-04 14:21:54 -0300

    Feat: New function: sort_complex.c

--- srcs/sort/sort.c
+++ srcs/sort/sort.c
@@ -6,7 +6,7 @@
 /*   By: jpaulo-p <jpaulo-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 13:10:26 by vneves-c          #+#    #+#             */
-/*   Updated: 2026/08/01 00:45:49 by jpaulo-p         ###   ########.fr       */
+/*   Updated: 2026/08/03 18:49:35 by jpaulo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	sort(t_context *ctx)
 		sort_two(ctx);
 	else if (ctx->a.size == 3)
 		sort_three(ctx);
+	else if (ctx->a.size <= 4)
+		sort_four(ctx);
 	else if (ctx->a.size <= 5)
 		sort_five(ctx);
 	else if (ctx->mode == mode_simple)
--- srcs/sort/sort_complex.c
+++ srcs/sort/sort_complex.c
@@ -6,8 +6,48 @@
 /*   By: jpaulo-p <jpaulo-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 13:10:26 by vneves-c          #+#    #+#             */
-/*   Updated: 2026/07/30 15:21:15 by jpaulo-p         ###   ########.fr       */
+/*   Updated: 2026/08/03 19:52:12 by jpaulo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	count_bits(int n)
+{
+	int	bits;
+
+	bits = 0;
+	while (n > 0)
+	{
+		bits++;
+		n = n >> 1;
+	}
+	return (bits);
+}
+
+void	sort_complex(t_context *ctx)
+{
+	int	max_bits;
+	int	b;
+	int	i;
+	int	size;
+
+	max_bits = count_bits(ctx->a.size - 1);
+	b = 0;
+	while (b < max_bits)
+	{
+		size = ctx->a.size;
+		i = 0;
+		while (i < size)
+		{
+			if (((ctx->a.values[0] >> b) & 1) == 0)
+				pb(ctx);
+			else
+				ra(ctx);
+			i++;
+		}
+		while (ctx->b.size > 0)
+			pa(ctx);
+		b++;
+	}
+}
